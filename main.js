1369f894f92f93105ffd8c21bdfeacea1149e50f
Miron <myroshnychenk0@icloud.com>
2025-05-09 16:30:49 +0200

    home-work 20

--- main.js
+++ main.js
@@ -10,8 +10,12 @@ console.log('#3. JavaScript homework example file')
  * lastName - будь-яке прізвище, рядок
  * age - будь-який вік, число
  */
-
-// console.log(userObj)
+let userObj = {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 30
+}
+console.log(userObj)
 
 /*
  * #2
@@ -23,8 +27,10 @@ console.log('#3. JavaScript homework example file')
  * userObj.lastName  ← 'Smith'
  * userObj.fullName() → 'John Smith'.
  */
-
-// console.log(userObj.fullName()) // John Smith
+userObj.fullName = function () {
+    return this.firstName + ' ' + this.lastName;
+};
+console.log(userObj.fullName()) // John Smith
 
 /*
  * #3
@@ -35,9 +41,11 @@ console.log('#3. JavaScript homework example file')
  *
  * При виконанні завдання не використовуйте оператор if, потрібен розв'язок із логічним оператором ||.
  */
-
-// console.log(defUpperStr('My text')) // MY TEXT
-// console.log(defUpperStr())             // DEFAULT TEXT
+function defUpperStr(str) {
+    return (str || 'default text').toUpperCase();
+}
+console.log(defUpperStr('My text')) // MY TEXT
+console.log(defUpperStr())             // DEFAULT TEXT
 
 /*
  * #4
@@ -55,10 +63,18 @@ console.log('#3. JavaScript homework example file')
  * evenFn(15) → [2, 4, 6, 8, 10, 12, 14]
  * evenFn(20) → [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
  */
-
-// console.log(evenFn(10)) // [2, 4, 6, 8, 10]
-// console.log(evenFn(15)) // [2, 4, 6, 8, 10, 12, 14]
-// console.log(evenFn(20)) // [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+function evenFn(n) {
+    let arr = [];
+    for (let i = 2; i <= n; i++) {
+        if (i % 2 === 0) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+console.log(evenFn(10)) // [2, 4, 6, 8, 10]
+console.log(evenFn(15)) // [2, 4, 6, 8, 10, 12, 14]
+console.log(evenFn(20)) // [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 
 /*
  * #5
@@ -76,13 +92,32 @@ console.log('#3. JavaScript homework example file')
  * '2' → null
  * У реалізації функції обов'язково мають бути використані оператори switch / case / default.
  */
-
-// console.log(weekFn(1))   // 'Понеділок'
-// console.log(weekFn(3))   // 'Середа'
-// console.log(weekFn(7))   // 'Неділя'
-// console.log(weekFn(9))   // null
-// console.log(weekFn(1.5)) // null
-// console.log(weekFn('2')) // null
+function weekFn(n) {
+    switch (n) {
+        case 1:
+            return 'Понеділок';
+        case 2:
+            return 'Вівторок';
+        case 3:
+            return 'Середа';
+        case 4:
+            return 'Четвер';
+        case 5:
+            return 'П' + 'ятниця';
+        case 6:
+            return 'Субота';
+        case 7:
+            return 'Неділя';
+        default:
+            return null;
+    }
+}
+console.log(weekFn(1))   // 'Понеділок'
+console.log(weekFn(3))   // 'Середа'
+console.log(weekFn(7))   // 'Неділя'
+console.log(weekFn(9))   // null
+console.log(weekFn(1.5)) // null
+console.log(weekFn('2')) // null
 
 /*
  * #6
@@ -100,26 +135,34 @@ console.log('#3. JavaScript homework example file')
  * При виконанні завдання допускається використовувати тільки тернарний оператор ?.
  * Використання операторів if, switch - заборонено.
  */
+function ageClassification(n) {
+    return n > 0 && n <= 24 ? 'Дитинство' :
+        n > 24 && n <= 44 ? 'Молодість' :
+            n > 44 && n <= 65 ? 'Зрілість' :
+                n > 65 && n <= 75 ? 'Старість' :
+                    n > 75 && n <= 90 ? 'Довголіття' :
+                        n > 90 && n <= 122 ? 'Рекорд' :
+                            null;
+}
+console.log('    -1 :', ageClassification(-1)) // -1 : null
+console.log('     0 :', ageClassification(0)) // 0 : null
+console.log('     1 :', ageClassification(1)) // 1 : Дитинство
+console.log('    24 :', ageClassification(24)) // 24 : Дитинство
+console.log(' 24.01 :', ageClassification(24.01)) // 24.01 : Молодість
+console.log('    44 :', ageClassification(44)) // 44 : Молодість
+console.log(' 44.01 :', ageClassification(44.01)) // 44.01 : Зрілість
+console.log('    65 :', ageClassification(65)) // 65 : Зрілість
+console.log('  65.1 :', ageClassification(65.1)) // 65.1 : Старість
+console.log('    75 :', ageClassification(75)) // 75 : Старість
+console.log(' 75.01 :', ageClassification(75.01)) // 75.01 : Довголіття
+console.log('    90 :', ageClassification(90)) // 90 : Довголіття
+console.log(' 90.01 :', ageClassification(90.01)) // 90.01 : Рекорд
+console.log('   122 :', ageClassification(122)) // 122 : Рекорд
+console.log('122.01 :', ageClassification(122.01)) // 122.01 : null
+console.log('   150 :', ageClassification(150)) // 150 : null
 
-// console.log('    -1 :', ageClassification(-1)) // -1 : null
-// console.log('     0 :', ageClassification(0)) // 0 : null
-// console.log('     1 :', ageClassification(1)) // 1 : Дитинство
-// console.log('    24 :', ageClassification(24)) // 24 : Дитинство
-// console.log(' 24.01 :', ageClassification(24.01)) // 24.01 : Молодість
-// console.log('    44 :', ageClassification(44)) // 44 : Молодість
-// console.log(' 44.01 :', ageClassification(44.01)) // 44.01 : Зрілість
-// console.log('    65 :', ageClassification(65)) // 65 : Зрілість
-// console.log('  65.1 :', ageClassification(65.1)) // 65.1 : Старість
-// console.log('    75 :', ageClassification(75)) // 75 : Старість
-// console.log(' 75.01 :', ageClassification(75.01)) // 75.01 : Довголіття
-// console.log('    90 :', ageClassification(90)) // 90 : Довголіття
-// console.log(' 90.01 :', ageClassification(90.01)) // 90.01 : Рекорд
-// console.log('   122 :', ageClassification(122)) // 122 : Рекорд
-// console.log('122.01 :', ageClassification(122.01)) // 122.01 : null
-// console.log('   150 :', ageClassification(150)) // 150 : null
 
-/*
- Блок тестирования, везде должны быть true:
+ //Блок тестирования, везде должны быть true:
  console.log('    -1 :', ageClassification(-1) === null); // -1 : null
  console.log('     0 :', ageClassification(0) === null) // 0 : null
  console.log('     1 :', ageClassification(1) === 'Дитинство'); // 1 : Дитинство
@@ -136,7 +179,6 @@ console.log('#3. JavaScript homework example file')
  console.log('   122 :', ageClassification(122) === 'Рекорд'); // 122 : Рекорд
  console.log('122.01 :', ageClassification(122.01) === null); // 122.01 : null
  console.log('   150 :', ageClassification(150) === null); // 150 : null
-*/
 
 /*
  * #7
@@ -154,10 +196,17 @@ console.log('#3. JavaScript homework example file')
  * oddFn(15) → [1, 3, 5, 7, 9, 11, 13, 15]
  * oddFn(20) → [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
  */
-
-// console.log(oddFn(10)) // [1, 3, 5, 7, 9]
-// console.log(oddFn(15)) // [1, 3, 5, 7, 9, 11, 13, 15]
-// console.log(oddFn(20)) // [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+ function oddFn(n) {
+    let arr = [], i = 1;
+    while (i <= n) {
+        arr.push(i);
+        i += 2;
+    }
+    return arr;
+}
+console.log(oddFn(10)) // [1, 3, 5, 7, 9]
+console.log(oddFn(15)) // [1, 3, 5, 7, 9, 11, 13, 15]
+console.log(oddFn(20)) // [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 
 /*
  * #8
@@ -169,8 +218,20 @@ console.log('#3. JavaScript homework example file')
  *
  * Реалізуйте перевірку: якщо третім параметром передається не функція, потрібно повернути false.
  */
-
-// function mainFunc(a, b, cb) { }
+ function mainFunc(a, b, callback) {
+    if (typeof callback === 'function') {
+        return callback(a, b);
+    } else {
+        return false;
+    }
+}
+function mainFunc(a, b, cb) {
+    if (typeof cb === 'function') {
+        return cb(a, b);
+    } else {
+        return false;
+    }
+ }
 
 /*
  * Реалізуйте callback функції (cbRandom, cbPow, cbAdd) до основної функції (mainFunc), що повертатимуть відповідні результати обчислень.
@@ -179,13 +240,19 @@ console.log('#3. JavaScript homework example file')
 
 // cbRandom(a, b) - обчислює і повертає довільне ціле число в діапазоні між a і b включно.
 // function cbRandom(min, max) { }
-
+ function cbRandom(a, b) {
+    return Math.floor(Math.random() * (b - a + 1) + a);
+}
 // cbPow(a, b) - обчислює і повертає результат піднесення числа a у ступінь b.
 // function cbPow(num, pow) { }
-
+ function cbPow(a, b) {
+    return Math.pow(a, b);
+}
 // cbAdd(a, b) - обчислює і повертає суму двох чисел a і b.
 // function cbAdd(a, b) { }
-
+ function cbAdd(a, b) {
+    return a + b;
+}
 /*
  * mainFunc() повинна повертати результат роботи переданої їй поворотної функції, наприклад:
  * mainFunc(2, 5, cbRandom) → випадково від 2 до 5 включно
@@ -193,9 +260,10 @@ console.log('#3. JavaScript homework example file')
  * mainFunc(2, 5, cbPow) → 32
  * mainFunc(2, 5, cbAdd) → 7
  * mainFunc(2, 5, 'not a func') → false
- */
+ */ 
 
-// console.log(mainFunc(2, 5, cbRandom)) // цілі числа в діапазоні 2..5
-// console.log(mainFunc(2, 5, cbPow)) // 32
-// console.log(mainFunc(2, 5, cbAdd)) // 7
-// console.log(mainFunc(2, 5, 'not a func')) // false
+console.log(mainFunc(2, 5, cbRandom)) // цілі числа в діапазоні 2..5
+console.log(mainFunc(10, 30, cbRandom)); // випадково від 10 до 30 включно
+console.log(mainFunc(2, 5, cbPow)) // 32
+console.log(mainFunc(2, 5, cbAdd)) // 7
+console.log(mainFunc(2, 5, 'not a func')) // false
