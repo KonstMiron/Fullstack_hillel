ecb14dae7d191047ad8d78d17fbc77162edf38f3
Miron <myroshnychenk0@icloud.com>
2025-08-02 14:35:51 +0200

    Homework 39

---
+++ my-react-app/src/components/ClassComponent.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ClassComponent extends Component {
+  render() {
+    return <div>Це класовий компонент</div>;
+  }
+}
+
+export default ClassComponent;
